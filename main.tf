c2d595079e946c5f6081bc7e51f2e9b839fd78f0
nishantparsai <nishantparsai@gmail.com>
2025-03-14 16:51:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 resource "azurerm_resource_group" "om" {
     name = "shiva"
     location = "eastus"
-  
+
 }
+resource "azurerm_resource_group" "namah" {
+    name = "shiva-parvati"
+    location = "eastus"
+
+}
+
+resource "azurerm_resource_group" "shiv" {
+    name = "shiva-parvati-ganesha"
+    location = "eastus"
+
+}
+
+
